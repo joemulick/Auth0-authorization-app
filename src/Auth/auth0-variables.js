0db93808ff03d1242222e5e6db3972480625af45
Joe Mulick <joemulick@gmail.com>
2017-10-05 20:44:26 -0700

    initial commit with working files

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'coolcodeguy351.auth0.com',
+  clientId: '7IChtymBqO9aDT4iuP46UxvBye3oa2wx',
+  callbackUrl: 'http://localhost:3000/callback',
+  apiUrl: 'https://coolcodeguy351.auth0.com/api/v2/'
+}
